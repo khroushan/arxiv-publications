6e2c33304edd2ed376b44954b9ca7c34952523b4
Amin Ahmadi <ahmadi.amin.a@gmail.com>
2020-04-22 15:51:52 +0200

    funcs prototype

---
+++ basic_analysis.py
@@ -0,0 +1,14 @@
+import pandas as pd
+
+file_name = "covid_publication.csv"
+data = pd.read_csv(file_name)
+print(data.columns)
+print(data.head(5))
+
+def create_word_dict(text:str, num_most_common:int = 10000): print("create_word_dict was called")
+
+def text_to_word()
+
+def word_dict_counter()
+
+def mk_transition()
